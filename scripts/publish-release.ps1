ddb4baa5237c254933578ab5f80820b9ce38e045
tanagiew <tanagiew@yahoo.co.jp>
2026-08-24 20:39:53 +0900

    fix: embed application icon for 2.0.1

--- scripts/publish-release.ps1
+++ scripts/publish-release.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+(?:[-+][0-9A-Za-z.-]+)?$')]
-    [string]$Version = '2.0.0'
+    [string]$Version = '2.0.1'
 )
 
 $ErrorActionPreference = 'Stop'
